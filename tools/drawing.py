237c4a96e5ed3ae1a2f28788a0dce908e0865188
test <test@test.fr>
2024-06-18 12:10:34 +0200

    mask extraction change

--- tools/drawing.py
+++ tools/drawing.py
@@ -1,7 +1,9 @@
 import cv2 as cv
 import numpy as np
 import torch
+from kornia import create_meshgrid
 from kornia.feature.responses import harris_response
+from kornia.geometry import hflip, vflip
 from kornia.morphology import closing, opening, dilation, erosion
 from torch import Tensor, FloatTensor
 
@@ -23,13 +25,16 @@ def drawlines(img, lines, pts):
     return img
 
 
-def extract_roi_from_images(mask: ImageTensor, *args):
+def extract_roi_from_images(mask: ImageTensor, return_pts=True, *args):
     roi = []
     pts = []
     or_mask = 0
     and_mask = 1
     mask = ImageTensor(mask) if not isinstance(mask, ImageTensor) else mask
     mask = extract_external_occlusion(mask)
+    split_sides = split_point_closer_side(mask)
+    split_mask, left_grid, right_grid, top_grid, bottom_grid = split_sides
+
     if len(args) > 0:
         for m in args:
             m = ImageTensor(m) if not isinstance(m, ImageTensor) else m
@@ -39,53 +44,51 @@ def extract_roi_from_images(mask: ImageTensor, *args):
             and_mask *= m.GRAY()
 
     if not isinstance(or_mask, int):
-        m_transfo = [ImageTensor(mask.GRAY()).pad([1, 1, 1, 1]), ImageTensor(or_mask).pad([1, 1, 1, 1])]
-        m_roi = [ImageTensor(or_mask + mask.GRAY() > 0).pad([1, 1, 1, 1]),
-                 ImageTensor(and_mask * mask.GRAY() > 0).pad([1, 1, 1, 1])]
+        m_transfo = [ImageTensor(mask.GRAY()), ImageTensor(or_mask)]
+        m_roi = [ImageTensor(or_mask + mask.GRAY() > 0),
+                 ImageTensor(and_mask * mask.GRAY() > 0)]
     else:
-        m_transfo = [ImageTensor(mask.GRAY()).pad([1, 1, 1, 1])]
-        m_roi = [ImageTensor(or_mask + mask.GRAY() > 0).pad([1, 1, 1, 1])]
+        m_transfo = [ImageTensor(mask.GRAY())]
+        m_roi = [ImageTensor(or_mask + mask.GRAY() > 0)]
 
     for m_ in m_roi:
         m_ = m_.to_tensor().to(torch.float)
-        corner_map = harris_response(m_).squeeze()
-        center = int(corner_map.shape[0] / 2), int(corner_map.shape[1] / 2)
-        top_l = corner_map[:center[0], : center[1]]
-        top_r = corner_map[:center[0], center[1]:]
-        bot_l = corner_map[center[0]:, :center[1]]
-        bot_r = corner_map[center[0]:, center[1]:]
-        top_left = torch.argmax(top_l)
-        top_left = top_left // center[1] - 1, top_left % center[1] - 1
-        top_right = torch.argmax(top_r)
-        top_right = top_right // center[1] - 1, top_right % center[1] + center[1] - 1
-        bot_left = torch.argmax(bot_l)
-        bot_left = bot_left // center[1] + center[0] - 1, bot_left % center[1] - 1
-        bot_right = torch.argmax(bot_r)
-        bot_right = bot_right // center[1] + center[0] - 1, bot_right % center[1] + center[1] - 1
-        roi.append([int(max(top_left[0], top_right[0])), int(max(top_left[1], bot_left[1])),
-                    int(min(bot_left[0], bot_right[0])), int(min(bot_right[1], top_right[1]))])
+        left = m_ * (split_mask == 0)*left_grid
+        right = m_ * (split_mask == 1)*right_grid
+        top = m_ * (split_mask == 2)*top_grid
+        bottom = m_ * (split_mask == 3)*bottom_grid
 
-    for m_ in m_transfo:
-        m_ = m_.to_tensor().to(torch.float)
-        corner_map = harris_response(m_).squeeze()
-        center = int(corner_map.shape[0] / 2), int(corner_map.shape[1] / 2)
-        top_l = corner_map[:center[0], : center[1]]
-        top_r = corner_map[:center[0], center[1]:]
-        bot_l = corner_map[center[0]:, :center[1]]
-        bot_r = corner_map[center[0]:, center[1]:]
-        top_left = torch.argmax(top_l)
-        top_left = top_left % center[1] - 1, top_left // center[1] - 1
-        top_right = torch.argmax(top_r)
-        top_right = top_right % center[1] + center[1] - 1, top_right // center[1] - 1,
-        bot_left = torch.argmax(bot_l)
-        bot_left = bot_left % center[1] - 1, bot_left // center[1] + center[0] - 1
-        bot_right = torch.argmax(bot_r)
-        bot_right = bot_right % center[1] + center[1] - 1, bot_right // center[1] + center[0] - 1
-        pts.append([top_left, top_right, bot_left, bot_right])
+        roi.append([int(top.max()), int(left.max()),
+                    int(bottom_grid.max() - bottom.max()),
+                    int(right_grid.max() - right.max())])
+    if return_pts:
+        for m_ in m_transfo:
+            m_ = m_.to_tensor().to(torch.float)
+            corner_map = harris_response(m_).squeeze()
+            center = int(corner_map.shape[0] / 2), int(corner_map.shape[1] / 2)
+            top_l = corner_map[:center[0], : center[1]]
+            top_r = corner_map[:center[0], center[1]:]
+            bot_l = corner_map[center[0]:, :center[1]]
+            bot_r = corner_map[center[0]:, center[1]:]
+            top_left = torch.argmax(top_l)
+            top_left = top_left % center[1] - 1, top_left // center[1] - 1
+            top_right = torch.argmax(top_r)
+            top_right = top_right % center[1] + center[1] - 1, top_right // center[1] - 1,
+            bot_left = torch.argmax(bot_l)
+            bot_left = bot_left % center[1] - 1, bot_left // center[1] + center[0] - 1
+            bot_right = torch.argmax(bot_r)
+            bot_right = bot_right % center[1] + center[1] - 1, bot_right // center[1] + center[0] - 1
+            pts.append([top_left, top_right, bot_left, bot_right])
     if not isinstance(or_mask, int):
-        return roi[1], roi[0], FloatTensor(pts[0]), FloatTensor(pts[1])
+        if return_pts:
+            return roi[1], roi[0], FloatTensor(pts[0]), FloatTensor(pts[1])
+        else:
+            return roi[1], roi[0]
     else:
-        return roi[0], FloatTensor(pts[0])
+        if return_pts:
+            return roi[0], FloatTensor(pts[0])
+        else:
+            return roi[0]
 
 
 def extract_external_occlusion(mask: ImageTensor) -> Tensor:
@@ -94,7 +97,7 @@ def extract_external_occlusion(mask: ImageTensor) -> Tensor:
     new = mask.clone()
     kernel = torch.ones([5, 5], device=mask.device if isinstance(mask, Tensor) else 'cpu')
     for i in range(3):
-        temp = erosion(temp*1., kernel)
+        temp = erosion(temp * 1., kernel)
     for i in range(3):
         temp = dilation(temp, kernel)
     temp = dilation(temp, kernel)
@@ -103,48 +106,13 @@ def extract_external_occlusion(mask: ImageTensor) -> Tensor:
     return ImageTensor(new * mask.unpad())
 
 
-def extract_roi_from_map(mask_left: Tensor, mask_right: Tensor):
-    roi = []
-    pts = []
+def split_point_closer_side(mask: ImageTensor):
+    grid = create_meshgrid(mask.image_size[0], mask.image_size[1], device=mask.device)  # [1 H W 2]
+    left, top = grid[:, :, :, 0], grid[:, :, :, 1]
+    right, bottom = hflip(left), vflip(top)
+    conc = torch.cat([left, right, top, bottom], dim=0)
+    return (torch.argmin(conc, dim=0), (left + 1)/2*(mask.image_size[1]-1),
+            (right + 1)/2*(mask.image_size[1]-1),
+            (top + 1)/2*(mask.image_size[0]-1),
+            (bottom + 1)/2*(mask.image_size[0]-1))
 
-    m_roi = [ImageTensor(mask_right + mask_left > 0).pad([1, 1, 1, 1]),
-             ImageTensor(mask_right * mask_left > 0).pad([1, 1, 1, 1])]
-    m_transfo = [ImageTensor(mask_left).pad([1, 1, 1, 1]), ImageTensor(mask_right).pad([1, 1, 1, 1])]
-
-    for m_ in m_roi:
-        m_ = m_.to_tensor().to(torch.float)
-        corner_map = Tensor(harris_response(m_)).squeeze()
-        center = int(corner_map.shape[0] / 2), int(corner_map.shape[1] / 2)
-        top_l = corner_map[:center[0], : center[1]]
-        top_r = corner_map[:center[0], center[1]:]
-        bot_l = corner_map[center[0]:, :center[1]]
-        bot_r = corner_map[center[0]:, center[1]:]
-        top_left = torch.argmax(top_l)
-        top_left = top_left // center[1] - 1, top_left % center[1] - 1
-        top_right = torch.argmax(top_r)
-        top_right = top_right // center[1] - 1, top_right % center[1] + center[1] - 1
-        bot_left = torch.argmax(bot_l)
-        bot_left = bot_left // center[1] + center[0] - 1, bot_left % center[1] - 1
-        bot_right = torch.argmax(bot_r)
-        bot_right = bot_right // center[1] + center[0] - 1, bot_right % center[1] + center[1] - 1
-        roi.append([int(max(top_left[0], top_right[0])), int(max(top_left[1], bot_left[1])),
-                    int(min(bot_left[0], bot_right[0])), int(min(bot_right[1], top_right[1]))])
-
-    for m_ in m_transfo:
-        m_ = m_.to_tensor().to(torch.float)
-        corner_map = Tensor(harris_response(m_)).squeeze()
-        center = int(corner_map.shape[0] / 2), int(corner_map.shape[1] / 2)
-        top_l = corner_map[:center[0], : center[1]]
-        top_r = corner_map[:center[0], center[1]:]
-        bot_l = corner_map[center[0]:, :center[1]]
-        bot_r = corner_map[center[0]:, center[1]:]
-        top_left = torch.argmax(top_l)
-        top_left = top_left % center[1] - 1, top_left // center[1] - 1
-        top_right = torch.argmax(top_r)
-        top_right = top_right % center[1] + center[1] - 1, top_right // center[1] - 1,
-        bot_left = torch.argmax(bot_l)
-        bot_left = bot_left % center[1] - 1, bot_left // center[1] + center[0] - 1
-        bot_right = torch.argmax(bot_r)
-        bot_right = bot_right % center[1] + center[1] - 1, bot_right // center[1] + center[0] - 1
-        pts.append([top_left, top_right, bot_left, bot_right])
-    return roi[1], roi[0], FloatTensor(pts[0]), FloatTensor(pts[1])
