bafc6821d31b1ff3ddbabacf82c88512665be6ec
test <test@test.fr>
2024-06-18 15:14:52 +0200

    mask extraction change

--- Wrappers/DepthWrapper.py
+++ Wrappers/DepthWrapper.py
@@ -59,7 +59,7 @@ class DepthWrapper:
                                       post_process=post_process_depth,
                                       numpy=True,
                                       upsample=upsample)
-                depth_reg.im_name = image_src.im_name + '_depth'
+                depth_reg.name = image_src.name + '_depth'
                 conv_upsampling = MaxPool2d((3, 5), stride=1, padding=(1, 2), dilation=1)
                 conv_upsampling = Sequential(conv_upsampling)
                 res['depth_reg'] = depth_reg
