8f5df1be47c12e038c5bdc71226993c0caa03892
test <test@test.fr>
2024-09-03 15:46:11 +0200

    Learnable cameras are added

--- Cameras/__init__.py
+++ Cameras/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from .CameraSetup import CameraSetup
-from .Cameras import Camera
+from .Cameras import Camera, LearnableCamera
 from .StereoSetup import StereoSetup
 
 __all__ = ['Camera', 'StereoSetup', 'CameraSetup']
