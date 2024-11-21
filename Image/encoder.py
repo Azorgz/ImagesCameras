bc69941ae9253ef894b11411da216ce0eb2e217e
test <test@test.fr>
2024-11-21 13:11:50 +0100

    update in all

--- Image/encoder.py
+++ Image/encoder.py
@@ -79,6 +79,8 @@ class Decoder:
                 self.value = inp[..., [2, 1, 0]]
             elif inp.shape[-1] == 4:
                 self.value = inp[..., [2, 1, 0, 3]]
+                if all((self.value[..., -1] / 255).flatten().tolist()):
+                    self.value = inp[..., [0, 1, 2]]
             elif len(inp.shape) == 4:
                 self.batched = True
                 self.value = inp
@@ -96,6 +98,8 @@ class Decoder:
                 self.value = inp[..., [2, 1, 0]]
             elif inp.shape[-1] == 4:
                 self.value = inp[..., [2, 1, 0, 3]]
+                if all((self.value[..., -1] / 255).flatten().tolist()):
+                    self.value = inp[..., [0, 1, 2]]
             else:
                 self.value = inp
         assert self.value is not None, f'No Image found at {filename}'
