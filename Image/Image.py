6e0885f292b9c6f5a43856dae9c17dbe9f419971
test <test@test.fr>
2024-11-21 13:19:10 +0100

    update in resize

--- Image/Image.py
+++ Image/Image.py
@@ -504,8 +504,9 @@ class ImageTensor(Tensor):
         out = in_place_fct(self, in_place).reset_layers_order(in_place=False)
         if keep_ratio:
             ratio = torch.tensor(self.image_size) / torch.tensor(shape)
-            ratio = ratio.max()
-            out.data = F.interpolate(out.to_tensor(), (shape[ratio.argmax()], shape[ratio.argmax()]/ratio), mode='bilinear')#, scale_factor=float(1 / ratio))
+            out.data = F.interpolate(out.to_tensor(), (shape[ratio.argmax()], shape[ratio.argmax()] / ratio.max()),
+                                     mode='bilinear')  # , scale_factor=float(1 / ratio))
+
             out.pad([shape[0] - out.shape[-2], shape[1] - out.shape[-1]], in_place=True)
             out.image_size = out.shape[-2:]
         else:
