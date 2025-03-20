2c28fc95c2d537c056753592c6524194c7ff6512
test <test@test.fr>
2025-03-20 10:19:07 +0100

    update in Image

--- Metrics/__init__.py
+++ Metrics/__init__.py
@@ -1,26 +1,27 @@
 from __future__ import annotations
+import torch
+from .Metrics import SSIM, MultiScaleSSIM, MSE, RMSE, PSNR, PSNR, NEC, GradientCorrelation, SCC
 
-from .Metrics import Metric_ssim_tensor, MultiScaleSSIM_tensor, Metric_mse_tensor, Metric_rmse_tensor, \
-    Metric_psnr_tensor, Metric_nec_tensor
-import numpy as np
 
 # Version variable
 __version__ = "1.0"
 
-norms_dict = {'rmse': Metric_rmse_tensor,
-              'psnr': Metric_psnr_tensor,
-              'ssim': Metric_ssim_tensor,
-              'ms_ssim': MultiScaleSSIM_tensor,
-              'nec': Metric_nec_tensor}
+norms_dict = {'rmse': RMSE,
+              'psnr': PSNR,
+              'ssim': SSIM,
+              'ms_ssim': MultiScaleSSIM,
+              'nec': NEC,
+              'gc': GradientCorrelation,
+              'scc': SCC}
 
-stats_dict = {'mean': np.mean,
-              'std': np.std}
+stats_dict = {'mean': torch.mean,
+              'std': torch.std}
 
-__all__ = ["Metric_ssim_tensor",
-           "MultiScaleSSIM_tensor",
-           "Metric_mse_tensor",
-           "Metric_rmse_tensor",
-           "Metric_psnr_tensor",
-           "Metric_nec_tensor",
+__all__ = ["SSIM",
+           "MultiScaleSSIM",
+           "MSE",
+           "RMSE",
+           "PSNR",
+           "NEC",
            "norms_dict",
            "stats_dict"]
