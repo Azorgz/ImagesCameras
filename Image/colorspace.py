da0baf7c10be29e050274d06f4ff1d6a0a3c3704
test <test@test.fr>
2025-07-24 10:58:23 +0200

    update in Image colorspace LAB

--- Image/colorspace.py
+++ Image/colorspace.py
@@ -3,6 +3,7 @@ import torch
 from kornia.color import *
 from matplotlib import colormaps as cm
 from torch import Tensor
+from math import pi, sqrt, atan2, sin, exp
 
 from .utils import wrap_colorspace, switch_colormap
 
@@ -826,3 +827,71 @@ def colorspace_fct(colorspace_change):
     if wrapper is not None:
         fct = wrap_colorspace(wrapper, fct)
     return fct
+
+
+# The classic CIE ΔE2000 implementation, which operates on two L*a*b* colors, and returns their difference.
+# "l" ranges from 0 to 100, while "a" and "b" are unbounded and commonly clamped to the range of -128 to 127.
+def color_distance(img1, img2):
+    # Working in Python with the CIEDE2000 color-difference formula.
+    # k_l, k_c, k_h are parametric factors to be adjusted according to
+    # different viewing parameters such as textures, backgrounds...
+    img1 = img1.LAB() if  img1.colorspace != 'LAB' else img1
+    img2 = img2.LAB() if  img2.colorspace != 'LAB' else img2
+    l_1, a_1, b_1 = img1[:, :1]*100, (img1[:, 1:2]+1) * 255 / 2 - 128, (img1[:, 2:]+1) * 255 / 2 - 128
+    l_2, a_2, b_2 = img2[:, :1]*100, (img2[:, 1:2]+1) * 255 / 2 - 128, (img2[:, 2:]+1) * 255 / 2 - 128
+	k_l = k_c = k_h = 1.0
+	n = (sqrt(a_1 * a_1 + b_1 * b_1) + sqrt(a_2 * a_2 + b_2 * b_2)) * 0.5
+	n = n * n * n * n * n * n * n
+	# A factor involving chroma raised to the power of 7 designed to make
+	# the influence of chroma on the total color difference more accurate.
+	n = 1.0 + 0.5 * (1.0 - sqrt(n / (n + 6103515625.0)))
+	# Application of the chroma correction factor.
+	c_1 = sqrt(a_1 * a_1 * n * n + b_1 * b_1)
+	c_2 = sqrt(a_2 * a_2 * n * n + b_2 * b_2)
+	# atan2 is preferred over atan because it accurately computes the angle of
+	# a point (x, y) in all quadrants, handling the signs of both coordinates.
+	h_1 = atan2(b_1, a_1 * n)
+	h_2 = atan2(b_2, a_2 * n)
+	h_1 += 2.0 * pi * (h_1 < 0.0)
+	h_2 += 2.0 * pi * (h_2 < 0.0)
+	n = abs(h_2 - h_1)
+	# Cross-implementation consistent rounding.
+	if pi - 1E-14 < n and n < pi + 1E-14 :
+		n = pi
+	# When the hue angles lie in different quadrants, the straightforward
+	# average can produce a mean that incorrectly suggests a hue angle in
+	# the wrong quadrant, the next lines handle this issue.
+	h_m = (h_1 + h_2) * 0.5
+	h_d = (h_2 - h_1) * 0.5
+	if pi < n :
+		h_d += pi
+		# 📜 Sharma’s formulation doesn’t use the next line, but the one after it,
+		# and these two variants differ by ±0.0003 on the final color differences.
+		h_m += pi
+		# h_m += pi if h_m < pi else -pi
+	p = 36.0 * h_m - 55.0 * pi
+	n = (c_1 + c_2) * 0.5
+	n = n * n * n * n * n * n * n
+	# The hue rotation correction term is designed to account for the
+	# non-linear behavior of hue differences in the blue region.
+	r_t = -2.0 * sqrt(n / (n + 6103515625.0)) \
+			* sin(pi / 3.0 * exp(p * p / (-25.0 * pi * pi)))
+	n = (l_1 + l_2) * 0.5
+	n = (n - 50.0) * (n - 50.0)
+	# Lightness.
+	l = (l_2 - l_1) / (k_l * (1.0 + 0.015 * n / sqrt(20.0 + n)))
+	# These coefficients adjust the impact of different harmonic
+	# components on the hue difference calculation.
+	t = 1.0	+ 0.24 * sin(2.0 * h_m + pi * 0.5) \
+		+ 0.32 * sin(3.0 * h_m + 8.0 * pi / 15.0) \
+		- 0.17 * sin(h_m + pi / 3.0) \
+		- 0.20 * sin(4.0 * h_m + 3.0 * pi / 20.0)
+	n = c_1 + c_2
+	# Hue.
+	h = 2.0 * sqrt(c_1 * c_2) * sin(h_d) / (k_h * (1.0 + 0.0075 * n * t))
+	# Chroma.
+	c = (c_2 - c_1) / (k_c * (1.0 + 0.0225 * n))
+	# Returns the square root so that the DeltaE 2000 reflects the actual geometric
+	# distance within the color space, which ranges from 0 to approximately 185.
+	return sqrt(l * l + h * h + c * c + c * h * r_t)
+
