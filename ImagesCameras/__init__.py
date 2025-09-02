43f7bf637e35cb88e7910b9be7541cf044743f97
test <test@test.fr>
2025-09-02 15:13:37 +0200

    Change the structure to make it easy to install / use

--- ImagesCameras/__init__.py
+++ ImagesCameras/__init__.py
@@ -1,3 +1,4 @@
 __all__ = ["Cameras", "Image", "Geometry", "Visualizer", "tools", "Wrappers", "Metrics"]
 
-from .Image import ImageTensor
+from .Image import ImageTensor, DepthTensor
+from .Cameras import CameraSetup, Cameras
