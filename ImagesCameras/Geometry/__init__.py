8d05cf19b502242d52e342a30c9eb1f7ab316be1
test <test@test.fr>
2025-10-15 09:00:10 +0200

    clean the repo

--- ImagesCameras/Geometry/__init__.py
+++ ImagesCameras/Geometry/__init__.py
@@ -1,20 +1,7 @@
 from __future__ import annotations
-
-from .transforms import Compose, Pad, Unpad, Normalize, Resize, ResizeDepth, ResizeDisp, DispSide, ToTensor, \
-    ToFloatTensor
 from .KeypointsGenerator import KeypointsGenerator
 
-__all__ = ['Compose',
-           'Pad',
-           'Unpad',
-           'Normalize',
-           'Resize',
-           'ResizeDepth',
-           'ResizeDisp',
-           'DispSide',
-           'ToTensor',
-           'ToFloatTensor',
-           'KeypointsGenerator']
+__all__ = ['KeypointsGenerator']
 
 # Version variable
 __version__ = "1.0"
--- ImagesCameras/Wrappers/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-__all__ = ['DepthWrapper', 'DisparityWrapper']
-
-# Version variable
-__version__ = "1.0"
