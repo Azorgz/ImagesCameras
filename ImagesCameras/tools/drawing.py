3fdb0c8de8624fe61b5d5c834cbe418cfcb08bae
test <test@test.fr>
2026-04-01 13:51:48 +0200

    drawing update of implementation

--- ImagesCameras/tools/drawing.py
+++ ImagesCameras/tools/drawing.py
@@ -24,7 +24,7 @@ def draw_grid(im_cv, grid_size: int = 24):
     height, width = im_cv.shape
     color = (0, 0, 255)
     for x in range(0, width - 1, grid_size):
-        cv2.line(im_gd_cv, (x, 0), (x, height), color, 1, 1) # (0, 0, 0)
+        cv2.line(im_gd_cv, (x, 0), (x, height), color, 1, 1)  # (0, 0, 0)
     for y in range(0, height - 1, grid_size):
         cv2.line(im_gd_cv, (0, y), (width, y), color, 1, 1)
     im_gd_ts = kornia.utils.image_to_tensor(im_gd_cv / 255.).type(torch.FloatTensor).cuda()
@@ -47,12 +47,18 @@ def drawlines(img, lines, pts):
 
 
 def extract_roi_from_images(mask: ImageTensor, *args, return_pts=True):
+    """
+    :param mask:
+    :param args:
+    :param return_pts: return points of the corners of the ROI, in the order top_left, top_right, bot_left, bot_right
+    :return:
+    """
     roi = []
     pts = []
-    or_mask = 0
-    and_mask = 1
     mask = ImageTensor(mask) if not isinstance(mask, ImageTensor) else mask.clone()
-    mask = extract_external_occlusion(mask)
+    mask_valid = 1 - extract_external_occlusion(mask)
+    or_mask_valid = mask_valid * 1.
+    and_mask_valid = mask_valid * 1.
     split_sides = split_point_closer_side(mask)
     split_mask, left_grid, right_grid, top_grid, bottom_grid = split_sides
 
@@ -60,29 +66,31 @@ def extract_roi_from_images(mask: ImageTensor, *args, return_pts=True):
         for m in args:
             m = ImageTensor(m) if not isinstance(m, ImageTensor) else m.clone()
             assert m.image_size == mask.image_size
-            m = extract_external_occlusion(m)
-            or_mask += m.GRAY()
-            and_mask *= m.GRAY()
+            m = 1 - extract_external_occlusion(m)
+            or_mask_valid += m
+            and_mask_valid *= m
 
-    if not isinstance(or_mask, int):
-        m_transfo = [ImageTensor(mask.GRAY()), ImageTensor(or_mask)]
-        m_roi = [ImageTensor(or_mask + mask.GRAY() > 0),
-                 ImageTensor(and_mask * mask.GRAY() > 0)]
-    else:
-        m_transfo = [ImageTensor(mask.GRAY())]
-        m_roi = [ImageTensor(or_mask + mask.GRAY() > 0)]
+    and_mask_to_crop = (and_mask_valid == 0) * 1.0
+    or_mask_to_crop = (or_mask_valid == 0) * 1.0
+
+    m_transfo = [ImageTensor(or_mask_to_crop), ImageTensor(and_mask_to_crop)]
+    m_roi = [ImageTensor(or_mask_to_crop), ImageTensor(and_mask_to_crop)]
 
     for m_ in m_roi:
+        # first OR mask --> At least one image with information, second AND mask --> All images with information
         m_ = m_.to_tensor().to(torch.float)
-        left = m_ * (split_mask == 0)*left_grid
-        right = m_ * (split_mask == 1)*right_grid
-        top = m_ * (split_mask == 2)*top_grid
-        bottom = m_ * (split_mask == 3)*bottom_grid
+        if m_.any():
+            left = m_ * (split_mask == 0) * left_grid
+            right = m_ * (split_mask == 1) * right_grid
+            top = m_ * (split_mask == 2) * top_grid
+            bottom = m_ * (split_mask == 3) * bottom_grid
 
-        roi.append([int(left.max()),
-                    int(right_grid.max() - right.max()),
-                    int(top.max()),
-                    int(bottom_grid.max() - bottom.max())])
+            roi.append([int(left.max()),
+                        int(right_grid.max() - right.max()),
+                        int(top.max()),
+                        int(bottom_grid.max() - bottom.max())])
+        else:
+            roi.append([0, mask.image_size[1], 0, mask.image_size[0]])
     if return_pts:
         for m_ in m_transfo:
             m_ = m_.to_tensor().to(torch.float)
@@ -101,16 +109,10 @@ def extract_roi_from_images(mask: ImageTensor, *args, return_pts=True):
             bot_right = torch.argmax(bot_r)
             bot_right = bot_right % center[1] + center[1] - 1, bot_right // center[1] + center[0] - 1
             pts.append([top_left, top_right, bot_left, bot_right])
-    if not isinstance(or_mask, int):
-        if return_pts:
-            return roi[1], roi[0], FloatTensor(pts[0]), FloatTensor(pts[1])
-        else:
-            return roi[1], roi[0]
+    if return_pts:
+        return roi[1], roi[0], FloatTensor(pts[0]), FloatTensor(pts[1])
     else:
-        if return_pts:
-            return roi[0], FloatTensor(pts[0])
-        else:
-            return roi[0]
+        return roi[1], roi[0]
 
 
 def extract_external_occlusion(mask: ImageTensor) -> Tensor:
@@ -120,6 +122,8 @@ def extract_external_occlusion(mask: ImageTensor) -> Tensor:
         new = flood(new, (0, 0))
         mask.data = Tensor(new).to(mask.device).unsqueeze(0).unsqueeze(0)
         mask.unpad(in_place=True)
+    else:
+        return mask * 0.
     return mask
 
 
@@ -128,13 +132,13 @@ def split_point_closer_side(mask: ImageTensor):
     left, top = grid[:, :, :, 0], grid[:, :, :, 1]
     right, bottom = hflip(left), vflip(top)
     conc = torch.cat([left, right, top, bottom], dim=0)
-    return (torch.argmin(conc, dim=0), (left + 1)/2*(mask.image_size[1]-1),
-            (right + 1)/2*(mask.image_size[1]-1),
-            (top + 1)/2*(mask.image_size[0]-1),
-            (bottom + 1)/2*(mask.image_size[0]-1))
+    return (torch.argmin(conc, dim=0), (left + 1) / 2 * (mask.image_size[1] - 1),
+            (right + 1) / 2 * (mask.image_size[1] - 1),
+            (top + 1) / 2 * (mask.image_size[0] - 1),
+            (bottom + 1) / 2 * (mask.image_size[0] - 1))
 
 
-def get_common_roi(img_a, img_b, mode: Literal['lrtb', 'xyxy', 'xywh']='lrtb', inner: bool = False):
+def get_common_roi(img_a, img_b, mode: Literal['lrtb', 'xyxy', 'xywh'] = 'lrtb', inner: bool = True):
     """
     Find the common ROI between two tensor (B, C, H, W).
     return ROIs of the form: Tensor(B, 4) with (y1, y2, x1, x2) if mode='lrtb', (x1, y1, x2, y2) if mode='xyxy' and (x, y, w, h) if mode='xywh'
@@ -151,7 +155,8 @@ def get_common_roi(img_a, img_b, mode: Literal['lrtb', 'xyxy', 'xywh']='lrtb', i
     rois = torch.zeros([B, 4], dtype=torch.int64, device=img_a.device)
     for b in range(B):
         common_mask = (mask_a[b] & mask_b[b]) * 1. if inner else (mask_a[b] | mask_b[b]) * 1.
-        common_mask = opening(common_mask.unsqueeze(0).unsqueeze(0), kernel=torch.ones((5, 5)).to(img_a.device)).squeeze()
+        common_mask = opening(common_mask.unsqueeze(0).unsqueeze(0),
+                              kernel=torch.ones((5, 5)).to(img_a.device)).squeeze()
         common_mask = ~extract_external_occlusion(ImageTensor(common_mask)).squeeze().to(torch.bool)
         coords = torch.nonzero(common_mask)
 
@@ -182,14 +187,14 @@ def crop_to_common_roi(img_a, img_b, inner=True):
     cls_b = img_b.__class__
     outer_rois, inner_rois = [], []
     for b in range(img_a.shape[0]):
-        outer_roi, inner_roi = extract_roi_from_images(img_a[b].sum(0) > 0, img_b[b].sum(0) > 0, return_pts=False)
+        inner_roi, outer_roi = extract_roi_from_images(img_a[b].sum(0) > 0, img_b[b].sum(0) > 0, return_pts=False)
         outer_rois.append(outer_roi)
         inner_rois.append(inner_roi)
     cropped_a = []
     cropped_b = []
     for b in range(img_a.shape[0]):
         x1, x2, y1, y2 = inner_rois[b] if inner else outer_rois[b]
-        cropped_a.append(img_a[b:b+1, :, y1:y2, x1:x2])
-        cropped_b.append(img_b[b:b+1, :, y1:y2, x1:x2])
+        cropped_a.append(img_a[b:b + 1, :, y1:y2, x1:x2])
+        cropped_b.append(img_b[b:b + 1, :, y1:y2, x1:x2])
     return cls_a(torch.cat(cropped_a)), cls_b(torch.cat(cropped_b))
 
